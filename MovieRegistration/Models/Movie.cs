564c8b0d253f73bdd57b23d6f2021e989305b877
=KateDatema <k8datema@gmail.com>
2021-06-29 16:29:05 -0400

    uploading project

---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public enum Genre
+        {
+            Drama,
+            Comedy,
+            Horror,
+            Romance,
+            Action
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genre GenreType {get; set;}
+        public int Year { get; set; }
+        public int RunTime { get; set; }
+    }
+}
